4e7f6d1de66ed58de03fb9f2f8929cb92169a8ca
ana.mf <froggitfroggers@gmail.com>
2026-04-22 22:17:09 -0300

    Continuação das aulas PHP

---
+++ 04_variaveis.php
@@ -0,0 +1,27 @@
+<?php
+$name = 'Carlos Ferreira';
+echo $name;
+echo '<br>';
+
+$name = 'João Carlos';
+echo $name;
+echo '<br>';
+
+$name = 12.2;
+echo $name;
+echo '<br>';
+
+$bandafavorita = 'Gorillaz';
+echo $bandafavorita;
+echo '<br>';
+
+$bandafavorita = 'Imagine Dragons';
+echo $bandafavorita;
+echo '<br>';
+
+$bandafavorita = 'The Long Faces';
+echo $bandafavorita;
+echo '<br>';
+
+
+?>
---
+++ 06_array.php
@@ -0,0 +1,20 @@
+<?php
+//$carros = ['Uno', 'Gol', 'Fusion', 'Civic'];
+//echo $carros[2];
+
+$carroUm = ['Uno', 'Fiat', 'Branco'];
+echo $carroUm[2];
+
+$carroUm =[
+
+'modelo' => 'Uno',
+'marca'  => 'Fiat',
+'cor'  => 'Branco',  
+
+];
+
+echo $carroUm['cor'];
+echo $carroUm['modelo'];
+echo $carroUm['marca'];
+
+?>
