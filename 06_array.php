756a92b482224ac34bdcde3dea7e40a353c37d4c
ana.mf <froggitfroggers@gmail.com>
2026-04-27 20:14:30 -0300

    Trabalhando com Arrays

--- 06_array.php
+++ 06_array.php
@@ -17,4 +17,42 @@ echo $carroUm['cor'];
 echo $carroUm['modelo'];
 echo $carroUm['marca'];
 
+$carros_dois = [
+
+    'modelo' => 'Punto',
+    'marca' => 'Fiat',
+    'cor' => 'Amarelo',
+
+];
+
+
+$carro_tres = [
+
+'Modelo' => 'Fusca',
+'Marca' => 'Volkswagen',
+'cor' => 'Azul',
+
+];
+
+$carro_quatro = [
+
+'Modelo' => 'Gol',
+'Marca' => 'Volkswagen',
+'cor' => 'Vermelho',
+
+];
+echo '<br>';
+
+echo 'Modelo: ';
+echo $carro_quatro ['Modelo'];
+echo '<br>';
+echo 'marca: ';
+echo $carro_quatro ['Marca'];
+echo '<br>';
+echo 'cor: ';
+echo $carro_quatro ['cor'];
+ 
+
+
+
 ?>
