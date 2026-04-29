7a0685231907d6e1aae879c665f65ac843d2eef8
ana.mf <froggitfroggers@gmail.com>
2026-04-28 22:15:01 -0300

    Continuação...

---
+++ 11_manipulacao_arrays.php
@@ -0,0 +1,19 @@
+<?php
+echo '<pre>';
+$nomes = ['Rubens', 'Senac', 'Americana'];
+/*
+var_dump($nomes);
+$nomes[0] = strtoupper ($nomes[0]);
+$nomes[1] = strtoupper ($nomes[1]);
+$nomes[2] = strtoupper ($nomes[2]);
+var_dump($nomes);
+*/
+
+$nomes = array_map('transformarParaMaiuscula', $nomes);
+
+function transformarParaMaiuscula($valor)
+{
+    return strtoupper($valor);
+}
+var_dump($nomes);
+?>
