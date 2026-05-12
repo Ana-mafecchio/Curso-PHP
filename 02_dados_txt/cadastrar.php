a6031b046e994150999cca81d92eabb875a64e89
ana.mf <froggitfroggers@gmail.com>
2026-05-11 22:21:12 -0300

    Outro formulario

---
+++ 02_dados_txt/cadastrar.php
@@ -0,0 +1,9 @@
+<?php
+$nome_do_gato = $_POST['nome_do_gato'];
+$idade = $_POST['idade'];
+$castrado = $_POST['castrado'];
+$possui_doenca = $_POST['possui_doenca'];
+
+echo "$nome_do_gato, $idade, $castrado, $possui_doenca";
+
+?>
