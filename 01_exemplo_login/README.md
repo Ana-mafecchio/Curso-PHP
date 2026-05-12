43c68e3e95febac937575c3b4b51de76c43c43b1
ana.mf <froggitfroggers@gmail.com>
2026-05-11 21:26:33 -0300

    Validaçao login

---
+++ 01_exemplo_login/README.md
@@ -0,0 +1,1 @@
+Criamos um arquivo de formulario/login e validamos ele, e verificando se o email e a senha era do administrador.
--- 01_exemplo_login/formulario.php
+++ 01_exemplo_login/formulario.php
@@ -1,6 +1,5 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,11 +9,18 @@
     <h1>Login</h1>
     <form action="validar_login.php" method="post">
         <label>E-mail: </label>
-        <input type="email" name="email_usuario"><br>
+        <input type="email" name="email"><br>
         <label>Senha: </label>
         <input type="password" name="senha"><br>
         <button type="submit">Acessar</button>
         <button type="reset">Limpar</button>
     </form>
+   
+    <?php
+ 
+    echo $_GET['mensagem'] ?? "";
+ 
+    ?>
+   
 </body>
 </html>
